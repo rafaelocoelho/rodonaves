fd50c79ff6391a98bf77e8acd8b9217c860d4d1e
Rafael de O Coelho <rafael.oliveiracoelho@gmail.com>
2020-10-20 10:40:51 -0300

    Criação das models Cidade e Estado

---
+++ Models/Cidade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RodonavesAPI.Models
+{
+    public class Cidade
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Estado é obrigatório.")]
+        public int EstadoId { get; set; }
+
+        [
+            DisplayName("Cidade"),
+            Required(ErrorMessage = "O campo Cidade é obrigatório.")
+        ]
+        public string Descricao { get; set; }
+    }
+}
---
+++ Models/Estado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RodonavesAPI.Models
+{
+    public class Estado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [
+            DisplayName("Estado"),
+            Required(ErrorMessage = "O campo Estado é obrigatório.")
+        ]
+        public string Descricao { get; set; }
+
+        [
+            DisplayName("Sigla"),
+            Required(ErrorMessage = "O campo UF é obrigatório."),
+            MinLength(2, ErrorMessage = "O campo UF precisa ter no mínimo 2 caracteres."),
+            MaxLength(2, ErrorMessage = "O campo UF pode ter apenas 2 caracteres.")
+        ]
+        public string Sigla { get; set; }
+    }
+}
