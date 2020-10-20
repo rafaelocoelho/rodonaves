628f84391c9651a7746d148f1d96c5ff7605f4a3
Rafael de O Coelho <rafael.oliveiracoelho@gmail.com>
2020-10-20 10:53:32 -0300

    Criação da api/clientes
    Inclusão da controller ClientesController para fornecer os endpoints de um crud para api/clientes.

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using RodonavesAPI.Data;
+using RodonavesAPI.Models;
+
+namespace RodonavesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly RodonavesAPIContext _context;
+
+        public ClientesController(RodonavesAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clientes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetCliente()
+        {
+            return await _context.Cliente.Include(c => c.ClienteTelefones).ToListAsync();
+        }
+
+        // GET: api/Clientes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetCliente(int id)
+        {
+            var cliente = await _context.Cliente.Where(c => c.Id == id).Include(c => c.ClienteTelefones).ToListAsync();
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+
+            return cliente;        
+        }
+
+        // PUT: api/Clientes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCliente(int id, Cliente cliente)
+        {
+            if (id != cliente.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Cliente.Update(cliente);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw new SystemException("Não foi possível atualizar os registros.");
+                }
+            }
+
+            return CreatedAtAction("GetCliente", new { id = cliente.Id }, cliente);
+        }
+
+        // POST: api/Clientes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Cliente>> PostCliente(Cliente cliente)
+        {
+            _context.Cliente.Add(cliente);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCliente", new { id = cliente.Id }, cliente);
+        }
+
+        // DELETE: api/Clientes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Cliente>> DeleteCliente(int id)
+        {
+            var cliente = await _context.Cliente.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            var clienteTelefones = _context.ClienteTelefones.Where(c => c.Cliente == cliente);
+            _context.ClienteTelefones.RemoveRange(clienteTelefones);
+
+            _context.Cliente.Remove(cliente);
+            await _context.SaveChangesAsync();
+
+            return cliente;
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return _context.Cliente.Any(e => e.Id == id);
+        }
+    }
+}
